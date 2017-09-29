1fe76f77e6e6c1bef37e587d82fc3ae9b7604f75
Andreas Lydersen <andreas.lydersen@ntty.com>
2017-09-29 09:01:50 +0200

    Schema updated after corrections from ILFA, Dieter Guthardt

--- v1/run_test.rb
+++ v1/run_test.rb
@@ -3,25 +3,25 @@ require "circuitdata"
 
 
 # Test the Product
-#puts "Test the Product"
-#puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json' ))
+puts "Test the Product"
+puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json' ))
 
 # Test the Stackup
-#puts "Test the Stackup"
-#puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-stackup.json' ))
+puts "Test the Stackup"
+puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-stackup.json' ))
 
 # Test the Profile defaults
-#puts "Test the Profile defaults"
-#puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-profile-default.json' ))
+puts "Test the Profile defaults"
+puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-profile-default.json' ))
 
 # Test the Profile required
-#puts "Test the Profile enforced"
-#puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-profile-enforced.json' ))
+puts "Test the Profile enforced"
+puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-profile-enforced.json' ))
 
 # Test the Profile restricted
 puts "Test the Profile restricted"
 puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-profile-restricted.json' ))
 
 # Test the Capabilities
-#puts "Test the Capabilities"
-#puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-capability.json' ))
+puts "Test the Capabilities"
+puts JSON.pretty_generate(Circuitdata.compatibility_checker( 'testfile-product.json', 'testfile-capability.json' ))
